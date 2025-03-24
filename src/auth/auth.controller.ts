54f71ca9175a9a91186ddeb7a9a9d00abf69576f
Changsoo Cho <19483811+ccho21@users.noreply.github.com>
2025-03-24 19:01:09 -0400

    Update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,24 +1,41 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDto } from './dto/signup.dto';
 import { SigninDto } from './dto/signin.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { UserPayload } from './types/user-payload.type';
+import { Response } from 'express';
+import { JwtGuard } from 'src/common/guards/jwt.guard';
+import { GetUser } from 'src/common/decorators/get-user.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @UseGuards(JwtGuard)
+  @Get('me')
+  getMe(@GetUser() user: UserPayload) {
+    console.log('### what is returned?', user);
+    return user;
+  }
+
   @Post('signup')
-  signup(@Body() dto: SignupDto) {
-    return this.authService.signup(dto);
+  signup(@Body() dto: SignupDto, @Res({ passthrough: true }) res: Response) {
+    return this.authService.signup(dto, res);
   }
 
   @Post('signin')
-  signin(@Body() dto: SigninDto) {
-    return this.authService.signin(dto);
+  signin(@Body() dto: SigninDto, @Res({ passthrough: true }) res: Response) {
+    return this.authService.signin(dto, res);
   }
-
   @Get('google')
   @UseGuards(AuthGuard('google'))
   async googleAuth() {
@@ -27,7 +44,10 @@ export class AuthController {
 
   @Get('google/redirect')
   @UseGuards(AuthGuard('google'))
-  async googleRedirect(@Req() req: Request & { user: UserPayload }) {
-    return this.authService.googleSignin(req.user);
+  async googleRedirect(
+    @Req() req: Request & { user: UserPayload },
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    return this.authService.googleSignin(req.user, res);
   }
 }
