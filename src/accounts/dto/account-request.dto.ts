63883cf2137da879051756bc9a3414e49af19c6a
ccho21 <changsoo.charles.cho@gmail.com>
2025-04-18 21:27:42 -0400

    feat: add instructions

--- src/accounts/dto/account-request.dto.ts
+++ src/accounts/dto/account-request.dto.ts
@@ -1,61 +1,61 @@
 import {
-    ApiExtraModels,
-    ApiProperty,
-    ApiPropertyOptional,
-    PartialType,
-  } from '@nestjs/swagger';
-  import {
-    IsEnum,
-    IsInt,
-    IsNumber,
-    IsOptional,
-    IsString,
-    IsUUID,
-    Min,
-  } from 'class-validator';
-  import { AccountType } from '@prisma/client';
-  
-  export class BaseAccountRequestDTO {
-    @ApiProperty({ enum: AccountType })
-    @IsEnum(AccountType)
-    type: AccountType;
-  
-    @ApiProperty()
-    @IsString()
-    name: string;
-  
-    @ApiPropertyOptional()
-    @IsString()
-    @IsOptional()
-    color?: string;
+  ApiExtraModels,
+  ApiProperty,
+  ApiPropertyOptional,
+  PartialType,
+} from '@nestjs/swagger';
+import {
+  IsEnum,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Min,
+} from 'class-validator';
+import { AccountType } from '@prisma/client';
 
-    @ApiProperty({ description: '초기 잔액' })
-    @IsNumber()
-    @Min(0)
-    balance: number;
-  
-    @ApiPropertyOptional()
-    @IsString()
-    @IsOptional()
-    description?: string;
-  
-    @ApiPropertyOptional()
-    @IsInt()
-    @IsOptional()
-    settlementDate?: number;
-  
-    @ApiPropertyOptional()
-    @IsInt()
-    @IsOptional()
-    paymentDate?: number;
-  
-    @ApiPropertyOptional()
-    @IsOptional()
-    autoPayment?: boolean;
-  }
-  
-  @ApiExtraModels()
-  export class AccountCreateRequestDTO extends BaseAccountRequestDTO {}
-  
-  export class AccountUpdateRequestDTO extends PartialType(AccountCreateRequestDTO) {}
-  
+export class BaseAccountRequestDTO {
+  @ApiProperty({ enum: AccountType })
+  @IsEnum(AccountType)
+  type: AccountType;
+
+  @ApiProperty()
+  @IsString()
+  name: string;
+
+  @ApiPropertyOptional()
+  @IsString()
+  @IsOptional()
+  color?: string;
+
+  @ApiProperty({ description: '초기 잔액' })
+  @IsNumber()
+  @Min(0)
+  balance: number;
+
+  @ApiPropertyOptional()
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @ApiPropertyOptional()
+  @IsInt()
+  @IsOptional()
+  settlementDate?: number;
+
+  @ApiPropertyOptional()
+  @IsInt()
+  @IsOptional()
+  paymentDate?: number;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  autoPayment?: boolean;
+}
+
+@ApiExtraModels()
+export class AccountCreateRequestDTO extends BaseAccountRequestDTO {}
+
+export class AccountUpdateRequestDTO extends PartialType(
+  AccountCreateRequestDTO,
+) {}
