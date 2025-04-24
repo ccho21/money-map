138acae8fe14eded6ff3de05ea11117d2aefcd86
ccho21 <changsoo.charles.cho@gmail.com>
2025-04-24 09:21:34 -0400

    feat: temp

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,20 @@ import * as cookieParser from 'cookie-parser';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const allowedOrigins =
-  process.env.NODE_ENV === 'production'
-    ? ['https://money-map-prod.vercel.app']
-    : ['http://localhost:3001'];
+// const allowedOrigins =
+//   process.env.NODE_ENV === 'production'
+//     ? ['https://money-map-prod.vercel.app']
+//     : ['http://localhost:3001'];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   app.use(cookieParser());
   app.enableCors({
-    origin: allowedOrigins,
+    origin: [
+      'https://money-map-prod.vercel.app',
+      'https://money-app-front-ecru.vercel.app',
+    ],
     credentials: true,
   });
 
